a63502ef775af951854ec58f2b70b9dd3badde62
HyeokJung803 <jh010803@naver.com>
2025-11-04 11:03:06 +0900

    Response

---
+++ src/main/java/com/korit/servlet_study/ch03/UserRepository.java
@@ -0,0 +1,42 @@
+package com.korit.servlet_study.ch03;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+public class UserRepository {
+    private static UserRepository instance;
+    private List<User> users;
+    private Long autoId;
+
+    private UserRepository() {
+        users = new ArrayList<>();
+    }
+
+    public static UserRepository getInstance() {
+        if (Objects.isNull(instance)) {
+            instance = new UserRepository();
+        }
+        return instance;
+    }
+
+    public void insert(User user) {
+        user.setId(++autoId);
+        users.add(user);
+    }
+
+    public User findByUsername(String username) {
+        Optional<User> userOptional = users.stream()
+                .filter(user -> user.getUsername().equals(username))
+                .findFirst();
+
+
+        return null;
+    }
+
+    public List<User> findAll() {
+        return users;
+    }
+
+}
---
+++ src/main/java/com/korit/servlet_study/ch03/UserServlet.java
@@ -0,0 +1,76 @@
+package com.korit.servlet_study.ch03;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+@WebServlet("/ch03/users")
+public class UserServlet extends HttpServlet {
+    private UserRepository userRepository;
+    private ObjectMapper objectMapper;
+
+    @Override
+    public void init() throws ServletException {
+        userRepository = UserRepository.getInstance();
+        objectMapper = new ObjectMapper();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<User> users = userRepository.findAll();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding(StandardCharsets.UTF_8.name());
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(req.getInputStream()));
+//        StringBuilder builder = new StringBuilder();
+//        while (true) {
+//            String line = bufferedReader.readLine();
+//            if (Objects.isNull(line)) {
+//                break;
+//            }
+//            builder.append(line);
+//        }
+//        UserDto userDto = objectMapper.readValue(builder.toString(), UserDto.class);
+
+        UserDto userDto = objectMapper.readValue(req.getReader(), UserDto.class);
+        System.out.println(userDto);
+
+        User foundUser = userRepository.findByUsername(userDto.getUsername());
+
+        resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        resp.setContentType("application/json");
+
+        if (!Objects.isNull(foundUser)) {
+            ErrorResponse errorResponse = ErrorResponse.builder()
+                    .message("이미 존재하는 username입니다.")
+                    .build();
+            resp.setStatus(400);
+
+            objectMapper.writeValue(resp.getWriter(), errorResponse);
+            return;
+        }
+
+        User userEntity = userDto.toUser();
+        userRepository.insert(userEntity);
+
+        SuccessResponse<User> successResponse = SuccessResponse.<User>builder()
+                .message("사용자 등록을 완료하였습니다.")
+                .body(userEntity)
+                .build();
+        objectMapper.writeValue(resp.getWriter(), successResponse);
+    }
+}
